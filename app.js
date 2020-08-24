52d774b836b2b0f1745d524f84a9e31db60b320b
Anna Cheng <acheng0086@gmail.com>
2020-08-23 23:22:34 -0400

    API data working for first column

--- app.js
+++ app.js
@@ -1,25 +1,39 @@
 $(() => {
-    const $h1 = $('<h1>Outfit of The Day</h1>')
-    const $container = $("<div class='container'>")
-    const dayOfTheWeek = ["monday", "tuesday", "wednesday", "thursday", "friday"]
-    $("body").append($h1)
-    $("body").append($container)
+  const $container = $("<div class='container'>");
+  const dayOfTheWeek = ["monday", "tuesday", "wednesday", "thursday", "friday"];
+  $("body").append($container);
 
-    for (let i = 0; i < 5; i++){
-        const $weekDiv = $(`<div id="${dayOfTheWeek[i]}" class="week">${dayOfTheWeek[i].toUpperCase()}</div>`)
-        $container.append($weekDiv)
-    }
-    for (let i = 0; i < 5; i++){
-        const $forecastDiv = $(`<div id="${dayOfTheWeek[i]}" class="forecast"></div>`)
-        $container.append($forecastDiv)
-    }
-    for (let i = 0; i < 5; i++) {
-        const $outfitDiv = $(`<div id="${dayOfTheWeek[i]}" class="outfit"></div>`)
-        $container.append($outfitDiv)
-    }
-$.ajax({
-        url: "http://api.openweathermap.org/data/2.5/weather?appid=a7a2a762a83f880ce94286af0a41b891&q=Queens"
-}).then((data) => {
-        console.log(data)
-    })
+  for (let i = 0; i < 5; i++) {
+    const $weekDiv = $(
+      `<div id="${dayOfTheWeek[i]}" class="week">${dayOfTheWeek[
+        i
+      ].toUpperCase()}</div>`
+    );
+    $container.append($weekDiv);
+  }
+  for (let i = 0; i < 5; i++) {
+    const $forecastDiv = $(
+      `<div id="${dayOfTheWeek[i]}" class="forecast"></div>`
+    );
+    $container.append($forecastDiv);
+  }
+  for (let i = 0; i < 5; i++) {
+    const $outfitDiv = $(`<div id="${dayOfTheWeek[i]}" class="outfit"></div>`);
+    $container.append($outfitDiv);
+  }
+
+  $("form").on("submit", (event) => {
+    event.preventDefault();
+    const zipcode = $("input").val();
+
+    $.ajax({
+      url: `https://api.openweathermap.org/data/2.5/forecast?zip=${zipcode}&appid=8427b9145053fa92079d70aa4f4483ee`,
+    }).then((data) => {
+      $("#description").html(data.list[0].weather[0].description);
+      $("#tempHigh").html(data.list[0].main["temp_max"]);
+      $("#tempLow").html(data.list[0].main["temp_min"]);
+      $("#windSpeed").html(data.list[0].wind.speed);
+      $("#humidity").html(data.list[0].main.humidity);
+    });
+  });
 });
