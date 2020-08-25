c2994d975812bd34bb7f94e7ef91a067c3af1f13
Anna Cheng <acheng0086@gmail.com>
2020-08-24 22:46:25 -0400

    Got rid of table chart. Included data within the days of the week divs.

--- app.js
+++ app.js
@@ -8,20 +8,27 @@ $(() => {
 
   for (let i = 0; i < 5; i++) {
     const $weekDiv = $(
-      `<div id="${dayOfTheWeek[i]}" class="week">${dayOfTheWeek[
-        i
-      ].toUpperCase()}</div>`
+      `<div class="week">${dayOfTheWeek[i].toUpperCase()}</div>`
     );
     $container.append($weekDiv);
   }
+
   for (let i = 0; i < 5; i++) {
-    const $forecastDiv = $(
-      `<div id="${dayOfTheWeek[i]}" class="forecast"></div>`
-    );
+    const $forecastDiv = $(`<div id="${dayOfTheWeek[i]}" class="forecast"></div>`);
     $container.append($forecastDiv);
+      const $description = $(`<span>Description: </span><span id='description${i}' class="description"></span><br />`);
+      const $tempHigh = $(`<span>Temp High: </span><span id='tempHigh${i}' class='tempHigh'></span><br />`);
+      const $tempLow = $(`<span>Temp Low: </span><span id='tempLow${i}' class='tempLow'></span><br />`);
+      const $windSpeed = $(`<span>Wind Speed: </span><span id='windSpeed${i}' class='windSpeed'></span><br />`);
+      const $humidity = $(`<span>Humidity: </span><span id='humidity${i}' class='humidity'></span><br />`);
+
+      $forecastDiv.append($description, $tempHigh, $tempLow, $windSpeed, $humidity)
   }
+
   for (let i = 0; i < 5; i++) {
-    const $outfitDiv = $(`<div id="${dayOfTheWeek[i]}" class="outfit"></div>`);
+    const $outfitDiv = $(
+      `<div id="${dayOfTheWeek[i]}${i}"  class="outfit"></div>`
+    );
     $container.append($outfitDiv);
   }
 
