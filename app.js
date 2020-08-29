d6f293fd8b6872a1032c3dc81240aa6cd829840a
Anna Cheng <acheng0086@gmail.com>
2020-08-29 02:45:31 -0400

    icon image added

--- app.js
+++ app.js
@@ -1,36 +1,46 @@
 $(() => {
   const $container = $("<div class='week-container'>");
-  //CHANGE THE DAY OF THE WEEK TO BE DYNAMIC
   const $squares = ["one", "two", "three", "four", "five"];
-  const $closeButton = $("<br /><button id='close-button'>Close closet</button><br />")
+  const $closeButton = $("<br /><button id='close-button'>Close closet</button><br />");
   const fahrenheitSymbol = $("<span>&#8457;</span>");
   const percentageSymbol = $("<span>&#37;</span>");
+    const red = Math.floor(Math.random() * 256)
+    const green = Math.floor(Math.random() * 256)
+    const blue = Math.floor(Math.random() * 256)
 
-  $("body").append($container);
+    $("body").append($container);
+    
+    //CHANGE BACKGROUND COLOR FOR SUBMIT BUTTON
+    $("#submit").hover(() => {
+        $("#submit").css("background-color", `rgba(${red}, ${green}, ${blue}, 0.2)`)
+    })
+   
+  //CLOSET BUTTON
   $("#closet-button").on("click", () => {
     $("img").fadeIn(2000);
     $("#drag-msg").show();
     $("#closet-button").hide();
-    $('body').append($closeButton)
-      $closeButton.show()
+    $("body").append($closeButton);
+    $closeButton.show();
   });
-
+    
+  //CLOSE BUTTON
   $closeButton.on("click", () => {
-      $("img").fadeOut(1000);
-      $("#drag-msg").hide();
-      $("#closet-button").show()
-      $closeButton.hide()
+    $("img").fadeOut(1000);
+    $("#drag-msg").hide();
+    $("#closet-button").show();
+    $closeButton.hide();
   });
-
+    
+  //DATE DIVS
   for (let i = 0; i < 5; i++) {
     const $weekDiv = $(`<div class="week" id="date${i}">Date . . .</div>`);
     $container.append($weekDiv);
   }
-
+    
+  //FORECAST DIVS
   for (let i = 0; i < 5; i++) {
-    const $forecastDiv = $(
-      `<div id="${$squares[i]}" class="forecast"></div>`
-    );
+    const $forecastDiv = $(`<div id="${$squares[i]}" class="forecast"></div>`);
     const $description = $(
       `<span>Description: </span><span id='description${i}' class="description"></span><br />`
     );
@@ -46,23 +56,27 @@ $(() => {
     const $humidity = $(
       `<span>Humidity: </span><span id='humidity${i}' class='humidity'></span><br />`
     );
+      const $icon = $(`<span id ='icon${i}'></span>`)
     $container.append($forecastDiv);
     $forecastDiv.append(
       $description,
       $tempHigh,
       $tempLow,
       $windSpeed,
-      $humidity
+      $humidity,
+      $icon
     );
   }
-
+    
+  //DROPPABLE SQUARES BENEATH
   for (let i = 0; i < 5; i++) {
     const $outfitDiv = $(
       `<div id="${$squares[i]}${i}"  class="ui-widget-header droppable outfit"></div>`
     );
     $container.append($outfitDiv);
   }
-
+    
+  //DRAGGABLE & DROPPABLE FUNCTION
   $(function () {
     $(".draggable").draggable();
     $(".droppable").droppable({
@@ -74,13 +88,14 @@ $(() => {
       },
     });
   });
-
+    
+  //FORM TO SUBMIT YOUR ZIP CODE TO GRAB API
   $("form").on("submit", (event) => {
     event.preventDefault();
     const zipcode = $("input").val();
 
     $.ajax({
-        url: `https://api.weatherbit.io/v2.0/forecast/daily?&postal_code=${zipcode}&units=I&key=c48676848e2c4f5aa45c0fa1eeaf5e90`,
+      url: `https://api.weatherbit.io/v2.0/forecast/daily?&postal_code=${zipcode}&units=I&key=c48676848e2c4f5aa45c0fa1eeaf5e90`,
     }).then((info) => {
       for (let i = 0; i < info.data.length; i++) {
         $(`#date${i}`).html(info.data[i].valid_date);
@@ -88,7 +103,8 @@ $(() => {
         $(`#tempHigh${i}`).html(info.data[i].max_temp);
         $(`#tempLow${i}`).html(info.data[i].low_temp);
         $(`#windSpeed${i}`).html(info.data[i].wind_spd);
-        $(`#humidity${i}`).html(info.data[i].rh);
+          $(`#humidity${i}`).html(info.data[i].rh);
+          $(`#icon${i}`).html(`<img src='https://www.weatherbit.io/static/img/icons/${info.data[i].weather.icon}.png' width='80px'>`)
       }
       $(".tempHigh").append(fahrenheitSymbol);
       $(".tempLow").append(fahrenheitSymbol);
